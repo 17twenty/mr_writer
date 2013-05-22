07478a97794f0f36a7124fd6fa8a43643f14a2ab
Nick Glynn <n.s.glynn@gmail.com>
2013-05-22 16:52:19 +0100

    Some semblance of security added to stop naughty pwd exploits. Should probably use execl instead of system()

--- mr_writer.c
+++ mr_writer.c
@@ -2,11 +2,25 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <libgen.h>
 
-int main()
+int main(int argc, char **argv)
 {
-   setuid(0);
-   system("./mr_writer.py");
+    char link_path[1024];
+    char *full_path = NULL;
+    ssize_t len;
 
-   return 0;
+    len = readlink("/proc/self/exe", link_path, sizeof(link_path) - 1);
+    if (len != -1)
+        link_path[len] = '\0';
+    else {
+        perror("Readlink failed - ");
+        exit(1);
+    }
+
+    setuid(0);
+    asprintf(&full_path, "%s/mr_writer.py", dirname(link_path));
+    system(full_path);
+    free(full_path);
+    return 0;
 }
