902395787d0fac00707d9f87e76c38ae9e551cab
Nick Glynn <n.s.glynn@gmail.com>
2014-06-10 12:57:52 +0100

    Added basic image reading functionality
    Stylistic tweaks
    
    Added some sanity checking... This is horribly hacky :-/
    
    Merge branches
    
    Resolved merge issues
    
    Resolve conflict

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ EXEC = mr_writer
 all: $(EXEC)
 	-$(SILENT) $(CHOWN) root:root $(EXEC)
 	-$(SILENT) $(CHMOD) 4755 $(EXEC)
+	-$(SILENT) $(CHMOD) 755 $(EXEC).py
     
 clean:
 	rm -f $(EXEC)
--- mr_writer.py
+++ mr_writer.py
@@ -104,6 +104,126 @@ class ImageWriter(gtk.Dialog):
             print self.stderr
         self.finished = True
 
+class ImageReader(gtk.Dialog):
+    def __init__(self, parent, drives):
+        gtk.Dialog.__init__(self, self.__class__.__name__, parent,
+            0,
+            (gtk.STOCK_CLOSE, gtk.RESPONSE_NONE))
+        try:
+            self.set_screen(parent.get_screen())
+        except AttributeError:
+            self.connect('destroy', lambda *w: gtk.main_quit())
+        self.connect("response", lambda d, r: d.destroy())
+        
+        self.drive_list = drives
+        
+        if len(self.drive_list) != 1:
+            message = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK)
+            message.set_markup("Please ensure you have only 1 USB drive connected")
+            message.run()
+            gtk.main_quit()
+            return
+            
+        self.image_file = None
+        while not self.image_file:
+            returnedValue = self.getText()
+            print returnedValue
+            if len(returnedValue) > 3 and not returnedValue.endswith(".img"):
+                self.image_file = returnedValue + ".img"
+        
+	print self.image_file
+        
+        self.set_resizable(False)
+        self.still_working = False
+        
+        self.set_modal(True)
+        self.set_size_request(300, 180)
+    
+        self.finished = True
+        # Tell WM this is a dialog
+        self.set_type_hint (gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
+
+        # Tell WM this window belongs to parent
+        self.set_transient_for(parent) 
+
+        vbox = gtk.VBox(False, 5)
+        self.vbox.pack_start(vbox, True, True, 0)
+        vbox.set_border_width(5)
+
+        self.label = gtk.Label()
+        self.label_text = "Wait for it..."
+        self.label.set_line_wrap(True) 
+        vbox.pack_start(self.label, False, False, 0)
+
+        # Create our entry
+        self.progress = gtk.ProgressBar()
+        vbox.pack_start(self.progress, False, False, 0)
+        
+        gobject.timeout_add(400, self.pulse)
+        t = threading.Thread(target=self.read_thread)
+        t.setDaemon(True)
+        t.start()
+        self.show_all()
+        self.run()
+        
+    def pulse(self):
+        self.progress.pulse()
+        gtk.gdk.threads_enter()
+        self.label.set_text(self.label_text)
+        gtk.gdk.threads_leave()
+        if self.finished:    
+            self.progress.set_fraction(1.0)
+            self.label_text = "Finished!"
+            self.label.set_text(self.label_text)
+            return False
+        return True
+
+    def responseToDialog(self, entry, dialog, response):
+        dialog.response(response)
+    def getText(self):
+        #base this on a message dialog
+        dialog = gtk.MessageDialog(
+            None,
+            gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+            gtk.MESSAGE_QUESTION,
+            gtk.BUTTONS_OK,
+            None)
+        dialog.set_markup('Please specify the course code (e.g. EL504):')
+        #create the text input field
+        entry = gtk.Entry()
+        #allow the user to press enter to do ok
+        entry.connect("activate", self.responseToDialog, dialog, gtk.RESPONSE_OK)
+        #create a horizontal box to pack the entry and a label
+        hbox = gtk.HBox()
+        hbox.pack_start(gtk.Label("Course:"), False, 5, 5)
+        hbox.pack_end(entry)
+        #some secondary text
+        dialog.format_secondary_markup("This will store a file named <b>something</b>.img")
+        #add it and show it
+        dialog.vbox.pack_end(hbox, True, True, 0)
+        dialog.show_all()
+        #go go go
+        dialog.run()
+        text = entry.get_text()
+        dialog.destroy()
+        return text
+
+    def read_thread(self):
+        self.finished = False
+        
+        #for drive in self.drive_list:
+        self.label_text = "Reading %s from %s" % (self.image_file, self.drive_list[0])
+            
+        # The actual write process
+        command =  ["dd", "of=" + image_directory + self.image_file, "if="+ self.drive_list[0], "bs=8M"]
+        print command
+        sp = subprocess.Popen(command, shell=False, 
+             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        self.stdout,self.stderr = sp.communicate()
+        print self.stdout
+        print self.stderr
+        self.finished = True
 
 class MrWriter(gtk.Window):
     drive_list = []
@@ -112,7 +232,7 @@ class MrWriter(gtk.Window):
         gtk.Window.__init__(self)
         self.image_list = self.get_image_list()
         self.connect('destroy', lambda *w: gtk.main_quit())
-        self.set_title("USB Image Writer")
+        self.set_title("Mr Writer | Easy Image Reading/Writing")
         self.set_size_request(320, 160)
         self.set_position(gtk.WIN_POS_CENTER)
         vbox = gtk.VBox(spacing=10)
@@ -144,6 +264,11 @@ class MrWriter(gtk.Window):
         write_image_button = gtk.Button("Write Images")
         write_image_button.connect("clicked", self.on_write_image_clicked)
         self.go_box.attach(write_image_button, 1, 2, 0, 1)
+        
+        create_image_button = gtk.Button("Create Image")
+        create_image_button.connect("clicked", self.on_create_image_clicked)
+        self.go_box.attach(create_image_button, 0, 1, 0, 1)
+        
         vbox.pack_start(self.go_box, expand=False)
 
         self.add(vbox)
@@ -156,7 +281,10 @@ class MrWriter(gtk.Window):
         self.drive_label.set_text(
             "Ready to write to %d drives" % self.get_drive_count())
         self.show_all()
-
+        
+    def on_create_image_clicked(self, data):
+        image_reader = ImageReader(self, self.drive_list)
+        
     def on_write_image_clicked(self, data):
         # Get the image selected
 
@@ -177,6 +305,7 @@ class MrWriter(gtk.Window):
                         self.drive_list.append("/dev/" + drive)
 
         print "Found %d drives" % len(self.drive_list)
+        print self.drive_list
         return len(self.drive_list)
 
     def get_image_list(self):
@@ -193,8 +322,8 @@ if __name__ == "__main__":
     print "Running with eUID ", os.geteuid()
     gtk.gdk.threads_init()
     # Try and read the images directory
-    import ConfigParser as cp
     try:
+        import ConfigParser as cp
         config = cp.ConfigParser()
         tempConfig = config.read("./mr_writer.ini")
         IMAGE_DIRECTORY = config.get("General", "ImageDir")
@@ -205,7 +334,8 @@ if __name__ == "__main__":
         app = MrWriter()
         app.show()
         gtk.main()
-    except:
+    except Exception as e:
+        print "{}".format(e)
         message = gtk.MessageDialog(
             type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK)
         message.set_markup(
