4293add0b678eb8ce7e2189fd91a4756959acc47
Nick Glynn <n.s.glynn@gmail.com>
2014-06-10 12:57:52 +0100

    Added basic image reading functionality

--- mr_writer.py
+++ mr_writer.py
@@ -100,6 +100,117 @@ class ImageWriter(gtk.Dialog):
             print self.stderr
         self.finished = True
 
+class ImageReader(gtk.Dialog):
+    def __init__(self, parent, drives):
+        gtk.Dialog.__init__(self, self.__class__.__name__, parent,
+            0,
+            (gtk.STOCK_CLOSE, gtk.RESPONSE_NONE))
+        try:
+            self.set_screen(parent.get_screen())
+        except AttributeError:
+            self.connect('destroy', lambda *w: gtk.main_quit())
+        self.connect("response", lambda d, r: d.destroy())
+        
+        
+        self.image_file = self.getText() + ".img"
+        print self.image_file
+        
+        self.set_resizable(False)
+        self.still_working = False
+        
+        self.set_modal(True)
+        self.set_size_request(300, 180)
+    
+        self.finished = True
+        # Tell WM this is a dialog
+        self.set_type_hint (gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
+
+        # Tell WM this window belongs to parent
+        self.set_transient_for(parent) 
+
+        vbox = gtk.VBox(False, 5)
+        self.vbox.pack_start(vbox, True, True, 0)
+        vbox.set_border_width(5)
+
+        self.label = gtk.Label()
+        self.label_text = "Wait for it..."
+        self.label.set_line_wrap(True) 
+        vbox.pack_start(self.label, False, False, 0)
+
+        # Create our entry
+        self.progress = gtk.ProgressBar()
+        vbox.pack_start(self.progress, False, False, 0)
+        
+        self.drive_list = drives
+        
+        gobject.timeout_add(400, self.pulse)
+        t = threading.Thread(target=self.read_thread)
+        t.setDaemon(True)
+        t.start()
+        self.show_all()
+        self.run()
+        
+    def pulse(self):
+        self.progress.pulse()
+        gtk.gdk.threads_enter()
+        self.label.set_text(self.label_text)
+        gtk.gdk.threads_leave()
+        if self.finished:    
+            self.progress.set_fraction(1.0)
+            self.label_text = "Finished!"
+            self.label.set_text(self.label_text)
+            return False
+        return True
+
+    def responseToDialog(self, entry, dialog, response):
+        dialog.response(response)
+    def getText(self):
+        #base this on a message dialog
+        dialog = gtk.MessageDialog(
+            None,
+            gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+            gtk.MESSAGE_QUESTION,
+            gtk.BUTTONS_OK,
+            None)
+        dialog.set_markup('Course/File name (e.g. EL504):')
+        #create the text input field
+        entry = gtk.Entry()
+        #allow the user to press enter to do ok
+        entry.connect("activate", self.responseToDialog, dialog, gtk.RESPONSE_OK)
+        #create a horizontal box to pack the entry and a label
+        hbox = gtk.HBox()
+        hbox.pack_start(gtk.Label("Name:"), False, 5, 5)
+        hbox.pack_end(entry)
+        #some secondary text
+        dialog.format_secondary_markup("This will create a file named <i>something</i>.img")
+        #add it and show it
+        dialog.vbox.pack_end(hbox, True, True, 0)
+        dialog.show_all()
+        #go go go
+        dialog.run()
+        text = entry.get_text()
+        dialog.destroy()
+        return text
+
+    def read_thread(self):
+        self.finished = False
+        # TODO: Prompt for a filename here
+        
+        #for drive in self.drive_list:
+        self.label_text = "Writing %s to drive %s" % (self.image_file, self.drive_list[0])
+            
+        # The actual write process
+        command =  ["dd", "of=" + image_directory + self.image_file, "if="+ self.drive_list[0], "bs=8M"]
+        print command
+        sp = subprocess.Popen(command, shell=False, 
+             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        self.stdout,self.stderr = sp.communicate()
+        print self.stdout
+        print self.stderr
+        self.finished = True
+
+
 class MrWriter(gtk.Window):
     drive_list = []
     def __init__(self):
@@ -138,6 +249,11 @@ class MrWriter(gtk.Window):
         write_image_button = gtk.Button("Write Images")
         write_image_button.connect("clicked", self.on_write_image_clicked)
         self.go_box.attach(write_image_button, 1, 2, 0, 1)
+        
+        create_image_button = gtk.Button("Create Image")
+        create_image_button.connect("clicked", self.on_create_image_clicked)
+        self.go_box.attach(create_image_button, 0, 1, 0, 1)
+        
         vbox.pack_start(self.go_box, expand=False)
         
         self.add(vbox)
@@ -149,6 +265,9 @@ class MrWriter(gtk.Window):
         print "Any more drives"
         self.drive_label.set_text("Ready to write to %d drives" % self.get_drive_count())
         self.show_all()
+        
+    def on_create_image_clicked(self, data):
+        image_reader = ImageReader(self, self.drive_list)
         
     def on_write_image_clicked(self, data):
         # Get the image selected
@@ -164,10 +283,13 @@ class MrWriter(gtk.Window):
         for drive in os.listdir("/dev/"):
             if drive.startswith("sd") and drive.isalpha():
                 for line in f:
-                    if drive in line:
+                    print line
+                    if drive in line and "/dev/"+drive not in self.drive_list:
+                        print drive
                         self.drive_list.append("/dev/" + drive)
         
         print "Found %d drives" % len(self.drive_list)
+        print self.drive_list
         return len(self.drive_list)
                 
     def get_image_list(self):
